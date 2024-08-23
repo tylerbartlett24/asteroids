88780fb02e0a8b8de766f8097e686e03b8c7f6c7
tylerbartlett24 <crazyquilt86@gmail.com>
2024-08-22 22:44:44 -0230

    Got a rotating triangle

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -12,11 +12,14 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
